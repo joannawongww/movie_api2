75691852aee7d0b37e5811a41c4cf15a7a97abae
joannawongww <joanna_1994@hotmail.com>
2023-07-05 11:56:11 +0800

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,16 +1,18 @@
 const passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
-    Models = require('./models'),
+    Models = require('./models.js'),
     passportJWT = require('passport-jwt');
 
 let Users = Models.User,
     JWTStrategy = passportJWT.Strategy,
     ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(new LocalStrategy({
+passport.use(new LocalStrategy(
+    {
     usernameField: 'Username',
     passwordField: 'Password'
-}, (username, password, callback) => {
+    }, 
+(username, password, callback) => {
     console.log(username + ' ' + password);
     Users.findOne({Username: username}, (error, user) => {
         if (error){
