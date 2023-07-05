e989a6f36f9b5ae501656d930c51abab1930adf5
joannawongww <joanna_1994@hotmail.com>
2023-07-05 14:12:44 +0800

    fix capitalisation

--- index.js
+++ index.js
@@ -369,6 +369,6 @@ app.use((err, req, res, next) => {
 //listen req
 const port = process.env.PORT || 8080;
 
-app.listen(port, '0.0.0.0',() => {
+app.listen(PORT, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
 });
