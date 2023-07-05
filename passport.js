a095d75cfe9ee791a9a721a1fd140347486a3e43
joannawongww <joanna_1994@hotmail.com>
2023-07-05 12:11:00 +0800

    Update passport.js

--- passport.js
+++ passport.js
@@ -7,7 +7,6 @@ let Users = Models.User,
     JWTStrategy = passportJWT.Strategy,
     ExtractJWT = passportJWT.ExtractJwt;
 
-// .FINDONE() NO LONGER ACCEPT CALLBACK
 // passport.use(new LocalStrategy(
 //     {
 //     usernameField: 'Username',
@@ -30,36 +29,6 @@ let Users = Models.User,
 //         return callback(null, user);
 //     });
 // }));
-
-// module.exports = (router) => {
-//     router.post('/login',
-//   // wrap passport.authenticate call in a middleware function
-//   function (req, res, next) {
-//     // call passport authentication passing the "local" strategy name and a callback function
-//     passport.authenticate('local', function (error, user, info) {
-//       // this will execute in any case, even if a passport strategy will find an error
-//       // log everything to console
-//       console.log(error);
-//       console.log(user);
-//       console.log(info);
-
-//       if (error) {
-//         res.status(401).send(error);
-//       } else if (!user) {
-//         res.status(401).send(info);
-//       } else {
-//         next();
-//       }
-
-//       res.status(401).send(info);
-//     })(req, res);
-//   },
-
-//   // function to call once successfully authenticated
-//   function (req, res) {
-//     res.status(200).send('logged in!');
-//   });
-// }
 
 passport.use(new LocalStrategy(
     {
